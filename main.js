f47c50e531d3bb270df6b9905331cd6e1cbaa22c
Filo Juarez <91501581+filocanseco@users.noreply.github.com>
2021-12-07 15:26:12 -0800

    Final

---
+++ main.js
@@ -0,0 +1,36 @@
+$('.menu-item').mouseenter(function(){
+    $('.drop-down-off').addClass('drop-down-on')
+
+
+  })
+
+  $('.menu-item').mouseleave(function(){
+    $('.drop-down-off').removeClass('drop-down-on')
+
+
+  })
+
+
+  $(document).ready(function(){
+    $('.next').on('click', function(){
+      var currentImg = $('.active');
+      var nextImg = currentImg.next();
+  
+      if(nextImg.length){
+        currentImg.removeClass('active').css('z-index', -10);
+        nextImg.addClass('active').css('z-index', 10);
+      }
+    });
+  
+    $('.prev').on('click', function(){
+      var currentImg = $('.active');
+      var prevImg = currentImg.prev();
+  
+      if(prevImg.length){
+        currentImg.removeClass('active').css('z-index', -10);
+        prevImg.addClass('active').css('z-index', 10);
+      }
+    });
+  });
+  
+
